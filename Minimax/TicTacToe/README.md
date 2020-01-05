3040899f336ebae141b145f179226f7b04bc69e0
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2020-01-05 18:37:01 -0500

    minimax working

--- Minimax/TicTacToe/README.md
+++ Minimax/TicTacToe/README.md
@@ -1,2 +1,6 @@
 ## Tic Tac Toe
 https://www.youtube.com/watch?v=yaPUl31nypk
+
+https://www.youtube.com/watch?v=trKjYdBASyQ
+
+https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/
--- Minimax/TicTacToe/source/index.ts
+++ Minimax/TicTacToe/source/index.ts
@@ -68,11 +68,18 @@ const setTurn = (index: number, letter: string) => {
 
 let opponentChoiceRandom = (): number => {
     let element = emptyQs()[Math.floor(Math.random() * emptyQs().length)];
-    return qNumId(element.id);
+
+    // REMOVE THIS
+    return opponentChoiceMinimaxAI();
+
+    //return qNumId(element.id);
 };
 
-let opponentBestMove = () => {
+let opponentChoiceMinimaxAI = (): number => {
     let board = convertElementsToBoard();
+    let bestMove = findBestMove(board);
+    let moveToBoxMapping:number[][] = [ [0, 1, 2], [3, 4, 5], [6, 7, 8] ];
+    return moveToBoxMapping[bestMove.row][bestMove.col];
 };
 
 let convertElementsToBoard = (): String[][] => {
@@ -129,3 +136,143 @@ const disableListeners = () => {
 };
 
 enableListeners();
+
+//---------------------
+// AI stuff
+
+class Move {
+    row: number;
+    col: number;
+
+    constructor(row: number, col: number) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+function isMovesLeft(board: String[][]): Boolean {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] == '') {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function evaluate(board: String[][]): number {
+    // Checking for Rows for X or O victory.
+    for (let row = 0; row < 3; row++) {
+        if (board[row][0] == board[row][1] &&
+            board[row][1] == board[row][2]) {
+            if (board[row][0] == 'O')
+                return +10;
+            else if (board[row][0] == 'X')
+                return -10;
+        }
+    }
+
+    // Checking for Columns for X or O victory.
+    for (let col = 0; col < 3; col++) {
+        if (board[0][col] == board[1][col] &&
+            board[1][col] == board[2][col]) {
+            if (board[0][col] == 'O')
+                return +10;
+
+            else if (board[0][col] == 'X')
+                return -10;
+        }
+    }
+
+    // Checking for Diagonals for X or O victory.
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+        if (board[0][0] == 'O')
+            return +10;
+        else if (board[0][0] == 'X')
+            return -10;
+    }
+
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+        if (board[0][2] == 'O')
+            return +10;
+        else if (board[0][2] == 'X')
+            return -10;
+    }
+
+    // Else if none of them have won then return 0
+    return 0;
+}
+
+function minimax(board: String[][], depth: number, isMaximizer: boolean): number {
+    let score = evaluate(board);
+
+    // If Maximizer has won the game
+    // return his/her evaluated score
+    if (score == 10)
+        return score;
+
+    // If Minimizer has won the game
+    // return his/her evaluated score
+    if (score == -10)
+        return score;
+
+    // If there are no more moves and
+    // no winner then it is a tie
+    if (!isMovesLeft(board))
+        return 0;
+
+    if (isMaximizer) {
+        let best = -1000;
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[0].length; j++) {
+                if (board[i][j]=='') {
+                    // Make the move
+                    board[i][j] = 'O';
+                    best = Math.max (
+                        best, minimax(board,depth + 1, !isMaximizer)
+                    );
+                    board[i][j] = '';
+                }
+            }
+        }
+        return best;
+    } else {
+        let best = 1000;
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[0].length; j++) {
+                if (board[i][j]=='') {
+                    board[i][j] = 'X';
+                    best = Math.min (
+                        best, minimax(board,depth + 1, !isMaximizer)
+                    );
+                    board[i][j] = '';
+                }
+            }
+        }
+        return best;
+    }
+}
+
+// Assume AI is maximizer
+function findBestMove(board: String[][]): Move {
+    let bestVal = -1000;
+    let bestMove = new Move(-1, -1);
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] == '') {
+                board[i][j] = 'O';
+                let moveVal = minimax(board, 0, false);
+                board[i][j] = '';
+                if (moveVal > bestVal) {
+                    bestMove.row = i;
+                    bestMove.col = j;
+                    bestVal = moveVal;
+                }
+            }
+        }
+    }
+    return bestMove;
+}
